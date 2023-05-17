75086c674168bc129bd665a32e2a62f31533848c
Emmmylou Geith <emmylou.geith@gmx.de>
2023-05-17 20:51:18 +0200

    tried out more with properties

--- OOP_BermudaSpiel_Var2_NoBoolean/src/bermudaspiel/Highscore.java
+++ OOP_BermudaSpiel_Var2_NoBoolean/src/bermudaspiel/Highscore.java
@@ -1,5 +1,7 @@
 package bermudaspiel;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -18,23 +20,25 @@ public class Highscore extends ArrayList<Score> {
 		return instance;
 	}
 
-//	public void load() {
-//
-//	}
-//
-//	public void save() {
-//		int key;
-//		for (int i = 0; i < size(); i++) {
-//			key = i;
-//		}
-//		String value = prop.setProperty("" + key + value);
-//		try {
-//			prop.store(new FileOutputStream(filename), "Kommentar");
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	public void add() {
-//		
-//	}
+	public void load(String key) {
+		String wert = prop.getProperty(key);
+	}
+
+	public void save(String score) {
+		int key;
+		for (int i = 0; i < size(); i++) {
+			key = i;
+		}
+		prop.setProperty("" + key, score);
+
+		try {
+			prop.store(new FileOutputStream(highscore.txt), "Kommentar");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void add() {
+
+	}
 }
