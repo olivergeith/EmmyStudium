48123339d002593507e8255738e56dcce564ec6f
Emmmylou Geith <emmylou.geith@gmx.de>
2023-05-24 15:09:08 +0200

    Adjazenzliste done

--- Algo/Algo_Übungszettel10/src/Graph_Adjazenzliste.java
+++ Algo/Algo_Übungszettel10/src/Graph_Adjazenzliste.java
@@ -4,47 +4,51 @@ import java.util.List;
 import java.util.Map;
 
 public class Graph_Adjazenzliste {
-    private Map<Integer, List<Integer>> adjacencyList;
+	private Map<Integer, List<Integer>> adjacencyList;
 
-    /*Klassen Konstruktor*/
-    public Graph_Adjazenzliste() {
-        this.adjacencyList = new HashMap<>();
-    }
+	/* Klassen Konstruktor */
+	public Graph_Adjazenzliste() {
+		this.adjacencyList = new HashMap<>();
+	}
 
-    /*Methode zum Knoten hinzufuegen*/
-    public void addVertex(int vertex) {
-        adjacencyList.put(vertex, new ArrayList<Integer>());
-    }
+	/* Methode zum Knoten hinzufuegen */
+	public void addVertex(int vertex) {
+		adjacencyList.put(vertex, new ArrayList<Integer>());
+	}
 
-    /*Methode zum Kanten hinzufuegen*/
-    public void addEdge(int source, int destination) {
-    	/*Hier Code hinzufuegen*/
-    }
+	/* Methode zum Kanten hinzufuegen */
+	public void addEdge(int source, int destination) {
+		/* Hier Code hinzufuegen */
+		adjacencyList.get(source).add(destination);
+		adjacencyList.get(destination).add(source);
+	}
 
-    /*Methode zum Ausgeben des Graphen auf der Konsole*/
-    public void printGraph() {
-    	/*Hier Code hinzufuegen*/
-    }
+	/* Methode zum Ausgeben des Graphen auf der Konsole */
+	public void printGraph() {
+		/* Hier Code hinzufuegen */
+		for (int i = 0; i < adjacencyList.size(); i++) {
+			System.out.println(i + " : " + adjacencyList.get(i));
+		}
+	}
 
-    
-    /****Main Methode - muss nicht veraendert werden****/
-    public static void main(String[] args) {
-        Graph_Adjazenzliste graph = new Graph_Adjazenzliste();
+	/**** Main Methode - muss nicht veraendert werden ****/
+	public static void main(String[] args) {
+		Graph_Adjazenzliste graph = new Graph_Adjazenzliste();
 
-        graph.addVertex(0);
-        graph.addVertex(1);
-        graph.addVertex(2);
-        graph.addVertex(3);
-        graph.addVertex(4);
+		graph.addVertex(0);
+		graph.addVertex(1);
+		graph.addVertex(2);
+		graph.addVertex(3);
+		graph.addVertex(4);
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(3, 4);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 4);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 4);
 
-        graph.printGraph();
-    }
+		graph.printGraph();
+	}
 }
--- Algo/Algo_Übungszettel10/src/Graph_Adjazenzmatrix.java
+++ Algo/Algo_Übungszettel10/src/Graph_Adjazenzmatrix.java
@@ -1,36 +1,37 @@
 public class Graph_Adjazenzmatrix {
-    private int numVertices;
-    private int[][] adjacencyMatrix;
+	private int numVertices;
+	private int[][] adjacencyMatrix;
 
-    /*Klassen Konstruktor*/
-    public Graph_Adjazenzmatrix(int numVertices) {
-        this.numVertices = numVertices;
-        this.adjacencyMatrix = new int[numVertices][numVertices];
-    }
+	/* Klassen Konstruktor */
+	public Graph_Adjazenzmatrix(int numVertices) {
+		this.numVertices = numVertices;
+		this.adjacencyMatrix = new int[numVertices][numVertices];
+	}
 
-    /*Methode zum Kanten hinzufuegen*/
-    public void addEdge(int source, int destination) {
-    	/*Hier Code hinzufuegen*/
-    }
+	/* Methode zum Kanten hinzufuegen */
+	public void addEdge(int source, int destination) {
+		/* Hier Code hinzufuegen */
 
-    /*Methode zum Ausgeben des Graphen auf der Konsole*/
-    public void printGraph() {
-    	/*Hier Code hinzufuegen*/
-    }
+	}
 
-    /****Main Methode - muss nicht veraendert werden****/
-    public static void main(String[] args) {
-        int numVertices = 5;
-        Graph_Adjazenzmatrix graph = new Graph_Adjazenzmatrix(numVertices);
+	/* Methode zum Ausgeben des Graphen auf der Konsole */
+	public void printGraph() {
+		/* Hier Code hinzufuegen */
+	}
 
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 4);
-        graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
-        graph.addEdge(3, 4);
+	/**** Main Methode - muss nicht veraendert werden ****/
+	public static void main(String[] args) {
+		int numVertices = 5;
+		Graph_Adjazenzmatrix graph = new Graph_Adjazenzmatrix(numVertices);
 
-        graph.printGraph();
-    }
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 4);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 4);
+
+		graph.printGraph();
+	}
 }
