2685e1eb5feeeb7e581b0c3bce7ab1194f09d350
Emmmylou Geith <emmylou.geith@gmx.de>
2023-06-08 20:23:22 +0200

    images

--- KlickSpiel/src/klickspiel/objekte/HaiObjekt.java
+++ KlickSpiel/src/klickspiel/objekte/HaiObjekt.java
@@ -2,6 +2,9 @@ package klickspiel.objekte;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class HaiObjekt implements Objekt {
 
@@ -12,10 +15,13 @@ public class HaiObjekt implements Objekt {
 		return 0;
 	}
 
+	BufferedImage img = ImageIO.read(madcap - england - chyme - zip - shirt11__56018.jpg);
+
 	@Override
 	public void paint(Graphics g, int xpos, int ypos, int boxBreite, int boxHoehe) {
 		g.setColor(Color.red);
 		g.fillOval(xpos * boxBreite, ypos * boxHoehe, boxBreite, boxHoehe);
+		g.drawImage(img, xpos, ypos, boxBreite, boxHoehe, null, null);
 	}
 
 }
